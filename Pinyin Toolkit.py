b9a3cb06194e59d9249efa3ef09f41294a17d8fd
Nick Cook <nick@n-line.co.uk>
2009-05-30 23:18:34 +0100

    0.04 dev 4.3 (repost)

--- Pinyin Toolkit.py
+++ Pinyin Toolkit.py
@@ -4,139 +4,31 @@
 ########################################################################
 ###                   Mandarin-Chinese Pinyin Toolkit                ### 
 ########################################################################
-# A Plugin for the Anki Spaced Repition learning system <http://ichi2.net/anki/>
+# A Plugin for the Anki Spaced Repition software http://ichi2.net/anki/
 
 # Version Details:
-pinyin_toolkit="0.04"
-CCDict_Ver="2009-05-16T00:23:12Z"
-HanDeDict_Ver="Sat May 16 00:16:20 2009"
-dictlanguage="German" # Sets dictionary to be used
+pinyin_toolkit="0.05 dev 4.3"
+CCDict_Ver="2009-05-29T05:46:28Z" # [n=84885] http://www.mdbg.net/chindict/chindict.php?page=cc-cedict
+HanDeDict_Ver="Sat May 30 00:20:38 2009" # [n=169500] http://www.chinaboard.de/chinesisch_deutsch.php?mode=dl&w=8
+CFDICT_Ver="Wed Jan 21 01:49:53 2009" # [n=593] http://www.chinaboard.de/fr/cfdict.php?mode=dl&w=8
+dictlanguage="en" # Sets language
+# full support for: "en" (english) and "de" (German)
+#     Note that there are some important differences between handedict and cc-cedict (Measures Words, Simp-Trad swap, etc)
+# partial support for: "fr" (French) [CFDICT is still small but google translate will pick up the slack]
+# google translate support for other languges (use language code: http://www.loc.gov/standards/iso639-2/php/code_list.php)
 
-"""
-This plugin adds several useful features for Mandarin decks:
-1 improved pinyin generation using a dictionary to guess the likely reading
-2 output tone marks instead of numbers
-3 colorize pinyin according to tone mark to assist visual learners
-4 look up the meaning of characters in English (or German in the German edition)
-(5) colorize character according to tone to assist visual learners [if enabled]
-(6) generate the appropriate "[sound:ni2.ogg][sound:hao3.ogg]" tags to give rudimentary text-to-speech [if enabled]
 
-The plugin replaces standard Mandarin generation, so there is no need to rename your model or model tags.
-Features 1 to 4 will work automatically out of the box. Features 5 and 6 are optional extras that require enabling.
-
-"""
-# Installation
-"""
-1) download using Anki shared-plugin download feature
-2) Ensure your model has the tag "Mandarin"
-3) Ensure your model has the fields "Expression", "Reading", and "Meaning" (or customise below)
-4) To active character colorization:
-  - ensure your model has a field called "Color" to populate with colored characters 
-  - edit your models to use the new field
-  - The best way to do this is test on plain characters, but show colored characters in answers or where you are not testing reading or tones. 
-5) To active sound tag generation:
-  - ensure your model has a field called "Audio"
-  - obtain audio files in the format "ni3.ogg", "hao3.ogg"
-  - you can download such files here http://www.chinese-lessons.com/download.htm
-  - note that commercial software (such as Wenlin) includes higher quality versions you can use
-  - place the files in your deck's media directory (keep a copy, as Anki wipes them in media checks)
-
-If you plan not to use features such as character colorisation or audio generation,
-you should turn them off in the settings section below.
-
-"""
-# Changelog
-
-# Version 0.04 (19/05/2009)  Nick Cook <nick@n-line.co.uk>  [http://www.n-line.co.uk]
-"""
-* Two versions are now being distributed: English (using CC-CEDICT) and German (using HanDeDict)
-   - Thanks to Rainer Menes for suggesting use of HanDeDict
-* New character colorization feature
-   - If enabled, colored hanzi will be generated and placed in a field called "Color"
-   - to get the benefit of this you should alter models in a way to make use of colored Hanzi
-* Fix to give better handle the formatting of dictionary entries
-   - customisable dictionary meaning separator (defaults to line break)
-* more reliable sound tag generation
-* automatic blanking feature if hanzi field is emptied and de-focused (makes input faster)
-   - audio field is ignored if longer than 40 characters (i.e. if Anki has improved or recorded audio) [a better way to do this is in the works]
-* strip html from expression field before doing anything (prevents formating bugs)
-* several minor fixes and improvements to general usage
-- 
-"""
-# Version 0.03[r] (12/05/2009)  Nick Cook <nick@n-line.co.uk>  [http://www.n-line.co.uk]
-"""
-* Simplified and traditional generation now happens each lookup (no need to chose one or the other)
-* automatic English generation from dictionary
-  - English will only be generated for exact matches (words) not phrases
-* automatic colorization of pnyin by tone
-* Text-to-speech feature (automatic auto generation)
-* support for turning off new features
-* tidy code (remove unused & rationalise messy bits)
-* update dictionary to latest CC-CEDICT
-  - fix bug from 谁 [actually bug is no longer relevant under new directory structure]
-  - fix problems with pinyin generation of "v"/"u:"
-* rearrange to more logical filenames (pinyinfetcher.py etc)
-* change dictionary structure for better management, easier updates, and to allow new features
-   - split dictionary into 3 parts (CC-CEDICT, supplementary, and user) [entries prioritised from latter to former]
-   - supplementary dictionary contains previously hard-coded entries and new data such as numbers
-"""
-# Version 0.02.5[r] (21/03/2009)   Nick Cook <nick@n-line.co.uk>   [http://www.n-line.co.uk]
-"""
-* Dictionary updated to use adapted version of CC-CEDICT
-* entries increasing from 44,783 to 82,941
-"""
-# Version 0.02[r] (03/2009)   Damien Elmes  [http://ichi2.net/]
-"""
-* Ported to Anki 0.9.9.6 and tidied up
-* Brian Vaughan no longer maintaing plugin
-"""
-# Version 0.01[r] (2008)    Brian Vaughan [http://brianvaughan.net]
-"""
-* Original release
-
-"[r]" means "Retroactive Version Number"
-
-Chinese-English CC-CEDICT, available at: <http://www.mdbg.net/chindict/chindict.php?page=cc-cedict>
-Licensing of CC-CEDICT is Creative Commons Attribution-Share Alike 3.0 <http://creativecommons.org/licenses/by-sa/3.0/>
-
-Chinese-German dictionary HanDeDict, available at: <http://www.chinaboard.de/chinesisch_deutsch.php?mode=dl&amp;sid=dd1ddd9fb952eb74300d3fd743394799&w=8>
-Licensing of HanDeDict is  Creative Commons Attribution-Share Alike 2.0 Germany <http://creativecommons.org/licenses/by-sa/2.0/de/>
-
-Previous versions of this plugin used an adapted version of the original cedict dictionary
-"""
-# Bug-Hunting To-Do-List & Future Development Plans
-"""
-Bugs & Tweaks
-- track down the bug that means character colorisation fails if audio generation off 
-- remove space at the end of colored pinyin [requires complex string manipulation]
-- remove space between punctuation in pinyin (functional result of way colored characters are produced)
-- remove the space between erhua "r" and main word in colored pinyin
-- prevent loss of punctuation when colorizing characters [a lot trickier than it sounds]
-- track down why HanDeDict doesn't always retrieve entries that are known to be there
-
-Future Feature List
-- bundle audio files with plugin
-- (check if audio file exists before putting in tag)
-- investigate Anki source to see if possible: import audio files from another directory (would solve check media wiping and make bundling audio simpler)
-- look at the "Allows numbers to match pinyin tone mark.pyc" plugin, seems much more efficient tone mark generation
-- English dictionary - add "(1)" "(2)" "(3)" instead of simple line spaces
-- where dictionary has measure words, automatically add them to a MW field and add "noun" to "Type" field
-- where dictionary contains "to " automatically ass "verb" to a field called "Type"
-- rewrite code to use a less complicated class and sub structure
-- rewrite code to perform a single dictionary search and assign to an array in the form: "word", "pinyin", "tones", "translation"
-- add a true pinyin mode and option switch (no breaks between sylabels in same words) [this will require a full index, as above]
-- change text parser to look for text in "/ /" and "[ ]" instead of using splits, there is a neat re.compile around somewhere for this
-
-"""
 ############### Settings Section ###############
 
 ### Options ###
 # Can be either True or False
 colorizePY=True 
 tonify=True
-dictionarygeneration=True
+DictionaryGeneration=True
+FallBackOnGoogleTranslate=True
 cochargen=True
 audiogen=True
+MWcutout=True # must have translate on (and be using a dictionary version)
 
 # seperator for meaning dictionary entries
 endictsep = "<br />"
@@ -145,13 +37,13 @@ endictsep = "<br />"
 # What type of audio files are you using?
 audioextension = ".mp3" 
 
-### Field Settings (must match your model) ###
-
-expressionField = "Expression"
-readingField = "Reading"
-meaningField ="Meaning"
+# Field Settings (should match your model) PY_TK will guess defaults if not presen #
+expressionField = "Chinese"
+readingField = "Pinyin"
+meaningField ="English"
 audioField = "Audio"
 colorField = "Color"
+MWField = "MW"
 
 # The following line controls which model tag is used (it must match your deck)
 # You should not need to change this setting
@@ -169,11 +61,13 @@ from pinyin import pinyinfetcher
 from anki.hooks import addHook, removeHook
 from anki.features.chinese import onFocusLost as oldHook
 
+import urllib,urllib2 # for google translate
+
 #define the convertor models as per options above
 converterPY = pinyinfetcher.PinyinFetcher(colorizePY,tonify)
 
-if(dictionarygeneration):
-    converterENG = pinyinfetcher.PinyinFetcher(False,False,True,False,endictsep,dictlanguage)
+if(DictionaryGeneration):
+    converterENG = pinyinfetcher.PinyinFetcher(False,False,True,False,dictlanguage)
 else:
     converterENG = None
     
@@ -192,7 +86,23 @@ def onFocusLost(fact, field):
         return
     if not anki.utils.findTag(modelTag, fact.model.tags):
         return
+    newinput=field.value
     try:
+        """
+        # Check user specified fields exist, if not then default to standard usage
+        if not (expressionField in currentCard.fact):
+            expressionField = 'Expression'
+        if not (readingField in currentCard.fact):
+            readingField = 'Reading'
+        if not (meaningField in currentCard.fact):
+            readingField = 'Meaning'
+        if not (colorField in currentCard.fact):
+            colorField = 'Color'
+        if not (audioField in currentCard.fact):
+            colorField = 'Audio'
+        """
+
+
         if not fact[expressionField]:
             fact[readingField] = u""
             fact[meaningField] = u""
@@ -201,30 +111,66 @@ def onFocusLost(fact, field):
             # DEBUGME - add a more rigerous check on audio fields here (having problems with ratio based approach)
             if audiolen < 40: #
                 fact[audioField] = u""
+            return
         if not fact[readingField] :
-            tmp = converterPY.getPinyin(field.value,False)
-            if tmp != field.value:
+            tmp = converterPY.getPinyin(newinput,False)
+            if tmp != newinput:
                 fact[readingField] = tmp.lower()
         if not fact[meaningField]:
-            tmp = converterENG.getPinyin(field.value,True)
-            if tmp != field.value:
-                fact[meaningField] = tmp
+            tmp = converterENG.getPinyin(newinput,True,False,False,dictlanguage).replace("CL:","MW:")
+             
+                        
+            if (MWcutout): # cut measure word and paste into MW field
+                MWpos = tmp.find("MW:")
+                if MWpos != -1:
+                    tmplen = len(tmp)
+                    MWcut = tmp[MWpos+5:tmplen].replace("["," - ").replace("]","")
+                    fact[MWField] = MWcut
+
+                    tmp=tmp[:MWpos-1]
+            
+            tmp=tmp.replace("/",endictsep) #convert dictionary spacers and return  
+            
+            if tmp == "":
+                if (FallBackOnGoogleTranslate):
+                    tmp=gTrans(newinput)
+            if tmp != newinput:
+                fact[meaningField] = tmp                     
         if not fact[audioField]:
-            tmp = converterAudio.getPinyin(field.value,False,False,False,audioextension)
-            if tmp != field.value:
+            tmp = converterAudio.getPinyin(newinput,False,False,False,audioextension)
+            if tmp != newinput:
                 fact[audioField] = tmp.lower()
         if not fact[colorField]:
-            tmp = converterColChar.getPinyin(field.value,False,True)
-            if tmp != field.value:
+            tmp = converterColChar.getPinyin(newinput,False,True)
+            if tmp != newinput:
                 fact[colorField] = tmp 
         return
     except:
         return 
-    
+
+
+#gtrans ######################################################################
+def gTrans(src):
+    url="http://translate.google.com/translate_a/t?client=t&text=%s&sl=%s&tl=%s"%(urllib.quote(src.encode('utf-8')),'zh-CN',dictlanguage)
+    con=urllib2.Request(url, headers={'User-Agent':'Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11'}, origin_req_host='http://translate.google.com')
+    try:
+        req=urllib2.urlopen(con)
+    except urllib2.HTTPError, detail:
+        return '<span style="color:gray">[Internet Error]</span>'
+    except urllib2.URLError, detail:
+        return '<span style="color:gray">[Internet Error]</span>'
+    ret=U''
+    for line in req:
+        line=line.decode('utf-8').strip()
+    ret=U' '.join( (ret,line) )
+    ret = unicode(eval(ret),'utf8') + '<br /><span style="color:gray"><small>[Google Translate]</small></span>'
+    return ret
+
+
+
 removeHook('fact.focusLost', oldHook)
 addHook('fact.focusLost', onFocusLost)
 mw.registerPlugin("Mandarin Chinese Pinyin Toolkit", 4)
-
 
 if __name__ == "__main__":
   print "This is a plugin for the Anki Spaced Repition learning system and cannot be run directly."
--- pinyinfetcher.py
+++ pinyinfetcher.py
@@ -38,20 +38,29 @@ class PinyinFetcher :
     def __init__( self,
                   colorize = True,
                   tonify = True,
-                  dictnotpy=False,audiogen=False,endictsep="<br />",diclang="English"):
+                  dictnotpy=False,audiogen=False,endictsep="<br />",dictlanguage="en"):
 
         plugindir = mw.pluginsFolder()
         plugindir = os.path.join(plugindir, "pinyin")
-
-        #Create main dictionary (check for German option, otherwise use English)
-        if diclang=="German":
-            dictmain = os.path.join(plugindir,'dict-handedict.txt')
-        else:
-            dictmain = os.path.join(plugindir,'dict-cc-cedict.txt')
-        #Then append / replace entries using supplemntary dictionary [some functionality requires this line!]
         dictsupp = os.path.join(plugindir,'dict-supplimentary.txt')
-        #Finally append the user dictionary
         dictuser = os.path.join(plugindir,'dict-userdict.txt')
+
+        if (dictnotpy): # if in translate mode then determine which language to use
+            dictindexline = 1
+            if dictlanguage=="en":
+                dictmain = os.path.join(plugindir,'dict-cc-cedict.txt')
+            elif dictlanguage=="de":
+                dictmain = os.path.join(plugindir,'dict-handedict.txt')
+            elif dictlanguage=="fr":
+                dictmain = os.path.join(plugindir,'dict-cfdict.txt')
+            else: # if no dictionary for this language use pinyin only dictionary (faster)
+                dictmain = os.path.join(plugindir,'dict-pinyin.txt')
+        else: #use HanDeDict in blanked mode if just pinyin (more entries)
+            dictindexline = 0
+            dictmain = os.path.join(plugindir,'dict-cc-cedict.txt')
+
+        #Create main dictionary (check for translateion language, otherwise use English)
+
         
         self.dictionary = dict()
           
@@ -71,67 +80,113 @@ class PinyinFetcher :
         
         # Set the index depending on whether Pinyin lookup (or Meaning lookup)
         self.dictionary = dict()
-        if(dictnotpy):
-            dictindexline = 1
-        else:
-            dictindexline = 0
+
 
         # This block builds an array in self.dictionary so that the resulting meaning can be looked up
         # The dictionary will be return pinyin or english depending on dictindexline
-        # As they are built sequentially the sequence of increasing importance is: user, supp, main (as it need to be to work)
         self.dictionary = self.dictbuild(dictmain,dictindexline,self.dictionary,endictsep)
-        self.dictionary = self.dictbuild(dictsupp,dictindexline,self.dictionary,endictsep)
         self.dictionary = self.dictbuild(dictuser,dictindexline,self.dictionary,endictsep)
+        self.dictionary = self.dictbuild(dictsupp,dictindexline,self.dictionary,endictsep)
 
 
-
-    def dictbuild(self,dictfile,dictindexline,localdictionary,endictsep="<br />"):
+    def dictbuild(self,dictfile,dictindexline,localdictionary,lookuplang="pinyin"):
         file = codecs.open(dictfile,"r",encoding='utf-8')
                 
-        for text in file :
-            # first split simp, trad, and everything else
-            line = text.split(None,2)       
-            if len(line) == 3 :
-                # then use the square bracket to split pinyin from meaning
-                tempstring = line[2].split("]",1)
-                #if this line of the dictionary has only pinyin (no english so split won't work) then define it as an array [bit of a fiddle] 
-                if len(tempstring) < 2:
-                    tempstring = [line[2],""]
-                # tidy English so that it looks pretty [I think German doesn't need this, check]
-                tempstring[1] = tempstring[1].strip("\n").replace("  "," ").replace(" /","/").replace("/ ","/").lstrip("/").rstrip("/").rstrip(" ")
-                tmp = tempstring[1].split("/") 
-                tempstring[1] = tempstring[1].replace("/",endictsep,(len(tmp)-2)).replace("/","")
-                
-                # save the dictionary value in the localdictionary array and then return it
-                if dictindexline <= len(tempstring) :
-                    localdictionary[ line[0] ] = tempstring[dictindexline].strip("[").strip("]")
-                    # index traditional as well
-                    if line[0] != line[1] :                    
-                        localdictionary[ line[1] ] = tempstring[dictindexline].strip("[").strip("]")
+        for text in file :            
+            # cut around first space to get simplified and traditional entry   
+            simptrad = text.split(None,1)
+            if not len(simptrad)==2:
+                continue
+            # extract the pinyin
+            pystartpos=text.find("[")+1
+            pyendpos=text.find("]")
+            pinyin=text[pystartpos:pyendpos]
+            
+            # extract the translation (from the second ] plus several few spaces
+            translationstart=pyendpos+3 # (to jump over the "] /")
+            translationend=len(text)-3
+            translation=text[translationstart:translationend]
 
-        file.close()
+            
+            # create an array containing the dictionary entry
+            dictionaryitem=[simptrad[0],simptrad[1],pinyin,translation]
+
+
+            n=dictindexline+2 #temp fix until I change the way it requests 
+            localdictionary[dictionaryitem[0]] = dictionaryitem[n]
+            localdictionary[dictionaryitem[1]] = dictionaryitem[n]
+
+
+            """        
+            # temporary transtition fiddle to make dictindexline work
+            if dictindexline==1:
+                lookuplang="english"
+            else:
+                lookuplang="pinyin" # i.e. lookup for 0, as before
+            lookuplang="pinyin"
+            
+
+            if lookuplang=="pinyin":
+                localdictionary[dictionaryitem[0]] = dictionaryitem[3]
+                localdictionary[dictionaryitem[1]] = dictionaryitem[3]
+            elif lookuplang=="swaphanzi":
+                localdictionary[dictionaryitem[0]] = dictionaryitem[1]
+                localdictionary[dictionaryitem[1]] = dictionaryitem[0]
+            else: # assume lookup translation if another language code            
+                localdictionary[dictionaryitem[0]] = dictionaryitem[4]
+                localdictionary[dictionaryitem[1]] = dictionaryitem[4]
+            """
+        file.close()            
         return localdictionary
 
-    def getPinyin(self, sentence,wordlookuponly=False,colchar=False,removetones=False,audioextension=".mp3") :
+        """# Old version of code [very messy] 
+        if len(simptrad) == 2 :
+            # then use the square bracket to split pinyin from meaning
+            tempstring = line[2].split("]",1)
+            #if this line of the dictionary has only pinyin (no english so split won't work) then define it as an array [bit of a fiddle] 
+            if len(tempstring) < 2:
+                tempstring = [line[2],""]
+            # tidy translated dictionary entry so that it looks pretty 
+            tempstring[1] = tempstring[1].strip("\n").replace("  "," ").replace(" /","/").replace("/ ","/").lstrip("/").rstrip("/").rstrip(" ")
+            tmp = tempstring[1].split("/") 
+            tempstring[1] = tempstring[1].replace("/",endictsep,(len(tmp)-2)).replace("/","")
+            
+            
+            
+            # save the dictionary value in the localdictionary array and then return it
+            if dictindexline <= len(tempstring) :
+                localdictionary[ line[0] ] = tempstring[dictindexline].strip("[").strip("]")
+                localdictionary[ line[1] ] = tempstring[dictindexline].strip("[").strip("]")
+        """
+
+
+    def getPinyin(self, sentence,wordlookuponly=False,colchar=False,removetones=False,audioextension=".mp3",dictlanguage="en") :
         if sentence == None or len( sentence ) == 0 :
             return u""
-        i=0;
-        assert type(sentence)==unicode
-        #strip html (otherwise formatting code breaks all lookups)
-        sentence=re.sub('<(?!(?:a\s|/a|!))[^>]*>','',sentence)
         reading = u""
+        assert type(sentence)==unicode
+        sentence=re.sub('<(?!(?:a\s|/a|!))[^>]*>','',sentence) #strip html (otherwise formatting code breaks all lookups)
+
         #iterate through the text
-        while i < len ( sentence ) :
+        i=0
+        senlen = len(sentence)
+        while i < senlen :
             word = u""
             j=i+1
-            
-            # if in English/German mode and a word has already been found (i.e. a phrase) then return nothing
-            if(wordlookuponly):
-                if reading != "":
+
+            #if it's not the start of the line, pad between pinyin,
+            #or after punctuation marks
+            if i != 0:
+                if i != senlen:
+                    if( reading != "" ) :
+                        reading += ' '
+
+            if(wordlookuponly): 
+                if reading != "": 
                     if i != 0:
                         reading = u""
                         return reading
-                        
+                                                
             #look for progressively bigger multi-syllabic chinese words                
             while j <= len ( sentence ) :
                 if self.dictionary.has_key( sentence[ i:j ] ) :
@@ -143,11 +198,9 @@ class PinyinFetcher :
                 # Add a space and a 5th tone so that it will be dealt with properly during colorisation
                 # This needs to be done as otherwise pinyin and character colorisation will be out of step
                 elif len ( sentence[ i:j ] ) == 1 :
-                    reading += " " + sentence[ i:j ] +5
+                    word = sentence[ i:j ] +5
                     #increment to the next character
-                    i=j
-                    break
-                    
+                    j+=1                    
                 else :
                     break
             if word != "" :
@@ -155,11 +208,6 @@ class PinyinFetcher :
                 reading += self.dictionary[ word ]
                 i+= len( word )            
             
-                #if it's not the start of the line, pad between pinyin,
-                #or after punctuation marks
-                if( reading != "" ) :
-                    reading += ' '
-
 
         if (colchar):        
             i=0
@@ -169,7 +217,7 @@ class PinyinFetcher :
             while i != len(pinyinsplit) :
                 j=i+1
                 t=1               
-                while t < 6:
+                while t < 6: # adds a tone number to each character followed by a space so that it can be recognised by the coloriser
                     s=str(t)
                     if pinyinsplit[i].endswith(s) :
                         reading += sentence[i:j] + s + " "
@@ -195,4 +243,5 @@ class PinyinFetcher :
         
         reading = reading.replace('</span> ','</span>') # remove the invisible double spaces created by character colorisation
         return reading
+
 
